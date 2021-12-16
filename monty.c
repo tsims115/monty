405d12bf8ad7ad567b18884814e0fe8e17242b56
Alex Leeper <alixvox@gmail.com>
2021-12-16 00:40:31 +0000

    Added get_function function, and a struct head to the main header.

--- monty.c
+++ monty.c
@@ -1,4 +1,4 @@
-#include "monty.h"
+a#include "monty.h"
 /**
  * main - interpret monty bytecodes from files
  * @ac: number of arguments
@@ -11,6 +11,8 @@ int main(int ac, char **av)
 	FILE *file;
 	char *buf;
 	char **argv;
+	stack_t *head;
+	int (*funct)(va_list *);
 	ssize_t n = 0;
 	int i = 0;
 
@@ -38,9 +40,37 @@ int main(int ac, char **av)
 			buf[strlen(buf) - 1] = '\0';
 		argv = splitter(buf);
 		for (i = 0; argv[i] != NULL; i++)
+		{
 			printf("%s\n", argv[i]);
+			funct = get_function(argv[0]);
+			n += funct(*head, argv[1]);
+		}
 		free(argv);
 	}
 	fclose(file);
 	return (0);
+}
+
+int (*get_function(char *s))(va_list * args)
+{
+	instruction_t functs[] = {
+		{"push", push},
+		{"pall", pAll},
+		{"pint", pInt},
+		{"pop", pop},
+		{"add", add},
+		{"swap", swap},
+		{"nop", nop}
+	};
+	int i = 0;
+	
+	while (functs[i].opcode != NULL)
+	{
+		if (strcmp(s, functs[i].opcode) == 0)
+		{
+			return (functs[i].f);
+		}
+		i++;
+	}
+return (NULL);
 }
